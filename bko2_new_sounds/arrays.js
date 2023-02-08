9855097a6b64b9e993efc2ea209cff474d1b8fbc
ebjacino <eboycejacino@gmail.com>
2023-02-08 11:59:52 -0500

    changed number of trials presented to 32 instead of 64
    each stimulus presented twice instead of 4 times

---
+++ bko2_new_sounds/arrays.js
@@ -0,0 +1,47 @@
+var stimuli_arrays = [
+[
+    { stimulus: "sound/lili.wav", imageLeft: "img/bouba_7_S.png", imageRight: "img/kiki_7_S.png" },
+    { stimulus: "sound/titi.wav", imageLeft: "img/kiki_7_L_R.png", imageRight: "img/bouba_7_L.png" },
+    { stimulus: "sound/MAMA.wav", imageLeft: "img/bouba_7_L_R.png", imageRight: "img/kiki_7_L_R.png" },///
+    { stimulus: "sound/fifi.wav", imageLeft: "img/bouba_7_L.png", imageRight: "img/kiki_7_L_R.png" },
+    { stimulus: "sound/KAKA.wav", imageLeft: "img/bouba_7_L_R.png", imageRight: "img/kiki_7_L.png" },
+    { stimulus: "sound/sisi.wav", imageLeft: "img/kiki_7_S.png", imageRight: "img/bouba_7_S_R.png" },
+    { stimulus: "sound/ZAZA.wav", imageLeft: "img/kiki_7_L.png", imageRight: "img/bouba_7_L_R.png" },
+    { stimulus: "sound/zizi.wav", imageLeft: "img/kiki_7_S_R.png", imageRight: "img/bouba_7_S.png" }, ////
+    { stimulus: "sound/FAFA.wav", imageLeft: "img/bouba_7_S.png", imageRight: "img/kiki_7_S.png" },
+    { stimulus: "sound/kiki.wav", imageLeft: "img/kiki_7_L_R.png", imageRight: "img/bouba_7_L_R.png" },
+    { stimulus: "sound/LALA.wav", imageLeft: "img/bouba_7_L_R.png", imageRight: "img/kiki_7_L.png" },
+    { stimulus: "sound/ZAZA.wav", imageLeft: "img/bouba_7_S_R.png", imageRight: "img/kiki_7_S_R.png" },
+    { stimulus: "sound/didi.wav", imageLeft: "img/kiki_7_S.png", imageRight: "img/bouba_7_S.png" },
+    { stimulus: "sound/LALA.wav", imageLeft: "img/bouba_7_S.png", imageRight: "img/kiki_7_S_R.png" },
+    { stimulus: "sound/SASA.wav", imageLeft: "img/kiki_7_L.png", imageRight: "img/bouba_7_L.png" },
+    { stimulus: "sound/mimi.wav", imageLeft: "img/kiki_7_L_R.png", imageRight: "img/bouba_7_L.png" },
+    { stimulus: "sound/kiki.wav", imageLeft: "img/bouba_7_L.png", imageRight: "img/kiki_7_L.png" },
+    { stimulus: "sound/MAMA.wav", imageLeft: "img/bouba_7_L.png", imageRight: "img/kiki_7_L.png" },
+    { stimulus: "sound/didi.wav", imageLeft: "img/kiki_7_S_R.png", imageRight: "img/bouba_7_S.png" },
+    { stimulus: "sound/KAKA.wav", imageLeft: "img/bouba_7_L.png", imageRight: "img/kiki_7_L_R.png" },
+    { stimulus: "sound/sisi.wav", imageLeft: "img/bouba_7_S_R.png", imageRight: "img/kiki_7_S_R.png" },
+    { stimulus: "sound/FAFA.wav", imageLeft: "img/kiki_7_L.png", imageRight: "img/bouba_7_L.png" },
+    { stimulus: "sound/SASA.wav", imageLeft: "img/bouba_7_S_R.png", imageRight: "img/kiki_7_S.png" },
+    { stimulus: "sound/fifi.wav", imageLeft: "img/kiki_7_S.png", imageRight: "img/bouba_7_S.png" },
+    { stimulus: "sound/TATA.wav", imageLeft: "img/bouba_7_L.png", imageRight: "img/kiki_7_L.png" },
+    { stimulus: "sound/DADA.wav", imageLeft: "img/kiki_7_L.png", imageRight: "img/bouba_7_L.png" },
+    { stimulus: "sound/mimi.wav", imageLeft: "img/bouba_7_L.png", imageRight: "img/kiki_7_L_R.png" },
+    { stimulus: "sound/TATA.wav", imageLeft: "img/bouba_7_S_R.png", imageRight: "img/kiki_7_S_R.png" },
+    { stimulus: "sound/lili.wav", imageLeft: "img/kiki_7_S.png", imageRight: "img/bouba_7_S_R.png" },
+    { stimulus: "sound/DADA.wav", imageLeft: "img/bouba_7_S_R.png", imageRight: "img/kiki_7_S.png" },
+    { stimulus: "sound/zizi.wav", imageLeft: "img/bouba_7_S_R.png", imageRight: "img/kiki_7_S_R.png" },
+    { stimulus: "sound/titi.wav", imageLeft: "img/kiki_7_S.png", imageRight: "img/bouba_7_S_R.png" }
+],
+
+
+
+
+
+[
+
+]
+
+]
+
+
